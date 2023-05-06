5dc88a9ef321e5bd13d9c3065e42410513a76452
adilmayank <adilmayank5894@gmail.com>
2023-05-06 22:48:25 +0530

    date again

--- journalModel.js
+++ journalModel.js
@@ -4,8 +4,13 @@ const { v4: uuid } = require('uuid')
 
 const JournalsSchema = new Schema({
   id: { type: String, required: true, default: uuid() },
-  date: { type: Number, required: true, default: Date.now },
+  date: { type: Number, required: true },
   body: { type: String, required: true },
+})
+
+JournalsSchema.pre("save",(next) => {
+  this.date = Date.now()
+  next()
 })
 
 const JournalsModel = mongoose.model('journals', JournalsSchema)
