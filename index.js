9956ed1e7a5bb914a7e42db032fb52bf53d5478d
adilmayank <adilmayank5894@gmail.com>
2023-05-06 21:53:38 +0530

    fixed origin link for cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(
   cors({
     origin: [
       'http://localhost:3000',
-      'https://journalfornidhi-backend.onrender.com',
+      'https://journalfornidhi.onrender.com',
     ],
   })
 )
