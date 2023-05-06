9adce8ab018f0f63ffebdd47de8b0d253c7d472f
adilmayank <adilmayank5894@gmail.com>
2023-05-06 23:09:58 +0530

    serverSelectionTimeoutMS: 60000

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ async function dbConnect() {
   return new Promise((resolve, reject) => {
     resolve(
       mongoose.connect(
-        `mongodb+srv://adilmayank5894:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}.mongodb.net/?retryWrites=true&w=majority`
+        `mongodb+srv://adilmayank5894:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}.mongodb.net/?retryWrites=true&w=majority`,
+        {
+          serverSelectionTimeoutMS: 60000,
+        }
       )
     )
   })
 }
 
-module.exports = {dbConnect}
+module.exports = { dbConnect }
